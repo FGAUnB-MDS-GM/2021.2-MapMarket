ee10e7073d8b0f7feacd43d9a5329c0f132035e9
PabloChristianno <pablochristianno@gmail.com>
2022-04-04 15:01:06 -0300

    Criação da Home Page

---
+++ appmarket/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <div className='TextoHome'>
+            <div className='title'><h1>Bem Vindo ao MapMarket!</h1></div>
+            <div className='texto'><h3>O MapMarket tem o objetivo<br/> de 
+                melhorar a comunicação<br/> de clientes com super<br/> meracado 
+                de uma maneira<br/> simples e intuitiva.</h3></div>
+        </div>
+    )
+}
+
+export default Home
