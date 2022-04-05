5c897c7e49fd88570ff64da04c68799350cde85b
PabloChristianno <pablochristianno@gmail.com>
2022-04-04 22:49:44 -0300

    Criação de páginas de login e cadastro

--- appmarket/src/Pages/Home.js
+++ appmarket/src/Pages/Home.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 function Home(){
     return(
-        <div className='TextoHome'>
-            <div className='title'><h1>Bem Vindo ao MapMarket!</h1></div>
-            <div className='texto'><h3>O MapMarket tem o objetivo<br/> de 
-                melhorar a comunicação<br/> de clientes com super<br/> meracado 
-                de uma maneira<br/> simples e intuitiva.</h3></div>
+
+        <div className='main'>
+            <header>
+                <meta charSet='UTF-8'/>
+                <link rel='stylesheet' href='../Styles/main.css'/>
+            </header>
+            <body>
+                <div className='TextoHome'>
+                    <div className='title'><h1>Bem Vindo ao MapMarket!</h1></div>
+                    <div className='texto'><h3>O MapMarket tem o objetivo<br/> de 
+                        melhorar a comunicação<br/> de clientes com super<br/> meracado 
+                        de uma maneira<br/> simples e intuitiva.</h3></div>
+                </div>
+                <div className='botoes'>
+                    <button className='btnSMercado'>Supermercado</button>
+                    <button className='btnCliente'>Cliente</button>
+                </div>
+            </body>
+            <footer>
+                <h3>© Copyright  MapMarket - All Rights Reserved</h3>
+            </footer>    
         </div>
+        
     )
 }
 
---
+++ appmarket/src/Pages/LoginSM.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function LoginSM(){
+    return(
+
+        <div className='main'>
+            <header>
+                <meta charSet='UTF-8'/>
+                <link rel='stylesheet' href='../Styles/main.css'/>
+            </header>
+            <body>
+                <div className='TextoHome'>
+                    <h1>Login</h1>
+                    <form>
+                        <label for='femail'>Email:</label><br/>
+                        <input type='text' id='femail' name='femail'></input>
+                        <label for='fsenha'>Senha:</label><br/>
+                        <input type='text' id='fsenha' name='fsenha'></input>
+                    </form>
+                    <div className='botoes'>
+                        <button className='btnEntrar'>Entrar</button>
+                    </div>
+                    <link href=''>Cadastre-se</link>
+                </div> 
+            </body>
+            <footer>
+                <h3>© Copyright  MapMarket - All Rights Reserved</h3>
+            </footer>    
+        </div>
+        
+    )
+}
+
+export default LoginSM
